7681265d4c4f1b1d3343c2d6056dba19a872e3a2
hanif <hanifsholihin884@gmail.com>
2025-10-17 05:55:09 +0700

    bug: error deployment

--- src/models/dbpostgres.js
+++ src/models/dbpostgres.js
@@ -2,13 +2,17 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  port: 5432,
-  database: "postgres",
-  password: process.env.DATABASE_PASSWORD
-});
+
+const config = {
+    connectionString: process.env.DATABASE_URL
+};
+
+if (process.env.DATABASE_URL) {
+    config.ssl = {
+        rejectUnauthorized: false
+    };
+}
+const pool = new Pool(config);
 
 pool.query("SELECT NOW()", (err, result) => {
   if (err) {
