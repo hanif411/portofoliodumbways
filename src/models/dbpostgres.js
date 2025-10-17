84126eae66b0cf83b99ea343a7a7a97dbe4a9baa
hanif <hanifsholihin884@gmail.com>
2025-10-17 08:29:59 +0700

    update bug image

--- src/models/dbpostgres.js
+++ src/models/dbpostgres.js
@@ -3,11 +3,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-host: process.env.DATABASE_HOST,
-port: 5432,
-database: "postgres",
-user: process.env.DATABASE_USER,
-password: process.env.DATABASE_PASSWORD
+connectionString: process.env.DATABASE_URL,
 });
 
 pool.query("SELECT NOW()", (err, result) => {
