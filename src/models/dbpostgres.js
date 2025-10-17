28fbe5211473c2dd42fb67c28c545f8030b5a87f
hanif <hanifsholihin884@gmail.com>
2025-10-17 07:35:32 +0700

    update bug production vercel

--- src/models/dbpostgres.js
+++ src/models/dbpostgres.js
@@ -2,17 +2,13 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-
-const config = {
-    connectionString: process.env.DATABASE_URL
-};
-
-if (process.env.DATABASE_URL) {
-    config.ssl = {
-        rejectUnauthorized: false
-    };
-}
-const pool = new Pool(config);
+const pool = new Pool({
+host: process.env.DATABASE_HOST,
+port: 5432,
+database: "postgres",
+user: process.env.DATABASE_USER,
+password: process.env.DATABASE_PASSWORD
+});
 
 pool.query("SELECT NOW()", (err, result) => {
   if (err) {
